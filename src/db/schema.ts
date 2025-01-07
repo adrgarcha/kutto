ac4b5ad0ffc4e019280172362f4cd6fbb6c533e9
adrgarcha <adrgarcha@alum.us.es>
2025-01-07 18:27:45 +0100

    fix: remove createdAt field from schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,7 @@
-import { sql } from 'drizzle-orm';
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const urlsTable = sqliteTable('urls_table', {
    id: int().primaryKey({ autoIncrement: true }),
-   createdAt: int('created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
    hash: text().unique(),
    fullUrl: text('full_url'),
    shortUrl: text('short_url'),
